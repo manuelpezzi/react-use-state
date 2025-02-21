08eea601b082d85c264805a2a93b7225b25bddb5
manuelpezzi <pezzimanuel@gmail.com>
2025-02-21 14:13:01 +0100

    parte statica terminata

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,5 +4,27 @@ import { useState } from "react";
 
 const Main = () => {
     const [selectedLanguageId, setSelectedLanguageId] = useState(null);
-
+    const handleButtonClick = (id) => {
+        setSelectedLanguageId(id);
+    }
+    return (
+        <main>
+            {/*bottoni per il linguaggio*/}
+            <div className="btn-group mb.-4" role="group">
+                {Languages.map(linguaggio => (
+                    <div key={linguaggio.id}>
+                        <button onClick={() => handleButtonClick(linguaggio.id)}>
+                            {linguaggio.title}
+                        </button>
+                        {selectedLanguageId === linguaggio.id && (
+                            <div>
+                                {linguaggio.description}
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </main>
+    )
 }
+export default Main;
