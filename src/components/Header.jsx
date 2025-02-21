dd00896ccccdc72c61deb5b7541673fd16e77476
manuelpezzi <pezzimanuel@gmail.com>
2025-02-21 14:51:40 +0100

    mod title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="container mt-5">
-            <h1 className='text-center mb-4'>Impara lo Sviluppo Web</h1>
+            <h1 className='text-center mb-4'>Learn Web Development</h1>
         </header>
     );
 };
